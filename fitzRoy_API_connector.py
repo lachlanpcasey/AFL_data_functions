d3db266088e2e619adfd1023ff434b164472aafc
lachlanpcasey <122522521+lachlanpcasey@users.noreply.github.com>
2023-06-14 22:34:57 +1000

    working functions

--- fitzRoy_API_connector.py
+++ fitzRoy_API_connector.py
@@ -1,2 +1,4 @@
 from functions import fetch_fixture, func_initialise, fetch_player_stats
+func_initialise()
 fetch_fixture(season=2020, round_number=2)
+fetch_player_stats(season=2020, round_number=2)
