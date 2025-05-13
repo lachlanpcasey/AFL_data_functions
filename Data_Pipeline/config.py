acfdd17374c72f59c6905e0481c9e63bf3a4ee79
lachlanpcasey <122522521+lachlanpcasey@users.noreply.github.com>
2025-05-13 20:53:57 +1000

    update config file

--- Data_Pipeline/config.py
+++ Data_Pipeline/config.py
@@ -1,4 +1,8 @@
+import os
+
+# Path to the current file
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
 gcs_bucket_name = 'afl-data'
-
-service_account_file = 'service_account_json_afl_pipeline.json'
+service_account_file = os.path.join(
+    current_dir, 'service_account_json_afl_pipeline.json')
